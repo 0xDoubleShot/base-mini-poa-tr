3252d5033f88b905ecde36cda183f1f5e9a538d0
Double Shot <140751740+0xDoubleShot@users.noreply.github.com>
2025-09-27 20:34:34 +0300

    Update POA.sol

--- contracts/POA.sol
+++ contracts/POA.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract POA is ERC721, Ownable {
+    uint256 public nextId;
+    mapping(address => bool) public claimed;
+
+    constructor() ERC721("ProofOfAttendance", "POA") {}
+
+    function mintPOA() external {
+        require(!claimed[msg.sender], "Already claimed");
+        claimed[msg.sender] = true;
+        _safeMint(msg.sender, nextId);
+        nextId++;
+    }
+}
 
